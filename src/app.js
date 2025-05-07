20e5b3a512711027d4a4bc6cd0b7d12172127daf
“prashant-vw” <prashant.handel@visionwaves.com>
2025-05-07 16:45:29 +0530

    initialize backend

---
+++ src/app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+
+
+const app = express();
+
+app.use((req, res) => {
+    res.send('Hello, World!');
+    console.log('req is ', req);
+})
+
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
